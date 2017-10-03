28359513c73c7dc9cc5848152f4c555fd13809ba
Will Carpenter <willcarpenteriii@gmail.com>
2017-10-03 10:56:28 -0600

    added morgan to view http hits

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express    = require('express'),
+      bodyParser = require('body-parser'),
+      cors       = require('cors'),
+      logger     = require('morgan')
 
 const app = express()
 const port = process.env.NODE || 5000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+app.use(logger('dev'))
 app.use(cors())
 
+
+
 app.use(function (req, res) {
-	res.setHeader('Content-Type', 'text/plain')
+	res.setHeader('Content-Type', 'text/json')
 	res.write('you posted:\n')
 	res.end(JSON.stringify(req.body, null, 2))
 })
+
 
 
 app.get('/', (req, res) => {
